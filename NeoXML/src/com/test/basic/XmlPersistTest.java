9e81a781d246538793eb3e461d1a38f11eb6b7ed
Rohith Pavithran <rohithtp@gmail.com>
2025-03-10 00:30:50 +0000

    Formatting changes only, no code changes.

--- NeoXML/src/com/test/basic/XmlPersistTest.java
+++ NeoXML/src/com/test/basic/XmlPersistTest.java
@@ -16,45 +16,45 @@ import com.compare.xml.factory.XmlParseFactory;
 public class XmlPersistTest {
 
 //In dev branch
-	@Test
-	public void testXmlPersist(){
-		Path configPath = Paths.get("neo4j.properties");
-		Path xmlPath = Paths.get("C:/Temp/allianz-s.xml");
-		//Path xmlPath = Paths.get("/home/nikhil/dev/xml-sample/discogs_20130801_labels.xml");
-		//Path xmlPath = Paths.get("/home/nikhil/dev/xml-sample/books-tiny.xml");
-		XmlParseFactory xmlParseFactory= new XmlParseFactory(configPath);
-		xmlParseFactory.convertXmlToGraph(xmlPath);
-	}
-	
-	public void testGraphTreeService(){
-		Path configFilePath = Paths.get("neo4j.properties");
+    @Test
+    public void testXmlPersist(){
+        Path configPath = Paths.get("neo4j.properties");
+        Path xmlPath = Paths.get("xmlTest.xml"); // Updated to use xmlTest.xml from the repo
+        //Path xmlPath = Paths.get("/home/nikhil/dev/xml-sample/discogs_20130801_labels.xml");
+        //Path xmlPath = Paths.get("/home/nikhil/dev/xml-sample/books-tiny.xml");
+        XmlParseFactory xmlParseFactory= new XmlParseFactory(configPath);
+        xmlParseFactory.convertXmlToGraph(xmlPath);
+    }
+    
+    public void testGraphTreeService(){
+        Path configFilePath = Paths.get("neo4j.properties");
 
-		XmlParseFactory.initialize(configFilePath);
-		GraphDatabaseService graphDb = Neo4jDatabaseHandler.getGraphDatabase();
-		
-		XmlTreeServiceGraph treeServiceGraph = new XmlTreeServiceGraph(graphDb);
-		System.out.println("-----Test for DOM like methods on XmlGraph----\n");
-		List<XmlElement> groupElements = treeServiceGraph.getTags("book");
-		System.out.println("Fetching all Book Nodes...");
-		for (XmlElement xmlElement : groupElements) {
-			System.out.println(xmlElement.getAtrributeString());
-		}
-		XmlElement firstElement = groupElements.get(0);
-		System.out.println("\nFetching children of the first Book Node : " + firstElement.getAtrributeString() + "...");
-		List<XmlElement> childrenElements = treeServiceGraph.getChildren(firstElement);
-		for (XmlElement xmlElement : childrenElements) {
-			System.out.println(xmlElement.getTagName()+ " : " + xmlElement.getTagValue());
-		}
-		XmlElement child = childrenElements.get(0);
-		System.out.println("\nFetching parent of the first child element : "+child.getTagName()+" : "+child.getTagValue() + "...");
-		XmlElement parentElement = treeServiceGraph.getParent(child);
-		System.out.println(parentElement.getTagName() + " : " + parentElement.getAtrributeString());
-		
-		System.out.println("\nFetching siblings of the first Book Node : " + firstElement.getAttributes());
-		List<XmlElement> elementSibling = treeServiceGraph.getSiblings(firstElement);
-		for (XmlElement xmlElement : elementSibling) {
-			System.out.println(xmlElement.getTagName() + " : " + xmlElement.getAtrributeString());
-		}
-	}
-	
+        XmlParseFactory.initialize(configFilePath);
+        GraphDatabaseService graphDb = Neo4jDatabaseHandler.getGraphDatabase();
+        
+        XmlTreeServiceGraph treeServiceGraph = new XmlTreeServiceGraph(graphDb);
+        System.out.println("-----Test for DOM like methods on XmlGraph----\n");
+        List<XmlElement> groupElements = treeServiceGraph.getTags("book");
+        System.out.println("Fetching all Book Nodes...");
+        for (XmlElement xmlElement : groupElements) {
+            System.out.println(xmlElement.getAtrributeString());
+        }
+        XmlElement firstElement = groupElements.get(0);
+        System.out.println("\nFetching children of the first Book Node : " + firstElement.getAtrributeString() + "...");
+        List<XmlElement> childrenElements = treeServiceGraph.getChildren(firstElement);
+        for (XmlElement xmlElement : childrenElements) {
+            System.out.println(xmlElement.getTagName()+ " : " + xmlElement.getTagValue());
+        }
+        XmlElement child = childrenElements.get(0);
+        System.out.println("\nFetching parent of the first child element : "+child.getTagName()+" : "+child.getTagValue() + "...");
+        XmlElement parentElement = treeServiceGraph.getParent(child);
+        System.out.println(parentElement.getTagName() + " : " + parentElement.getAtrributeString());
+        
+        System.out.println("\nFetching siblings of the first Book Node : " + firstElement.getAttributes());
+        List<XmlElement> elementSibling = treeServiceGraph.getSiblings(firstElement);
+        for (XmlElement xmlElement : elementSibling) {
+            System.out.println(xmlElement.getTagName() + " : " + xmlElement.getAtrributeString());
+        }
+    }
+    
 }
